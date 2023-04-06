f17dcf43700b24da432a1f2ba0846a341289c2c7
DenisGoubkine <90278421+DenisGoubkine@users.noreply.github.com>
2023-04-06 15:56:10 -0400

    falling platforms prototype

--- WhirlyBird/Assets/Scripts/Platform.cs
+++ WhirlyBird/Assets/Scripts/Platform.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    protected Rigidbody2D rb;
     public float jumpForce = 10f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.relativeVelocity.y <= 0f)
         {
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            
             if (rb!= null)
             {
                 Vector2 velocity = rb.velocity;
@@ -20,5 +27,7 @@ public class Platform : MonoBehaviour
         }
 
     }
+    
+
 }
 
